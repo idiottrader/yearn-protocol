1e9f90594a53ed68e3201920b1ad8c1c8c9ceb91
idiottrader <429136075@qq.com>
2020-11-21 20:10:21 +0800

    modify StrategyYFIGovernance.sol

--- contracts/StrategyYFIGovernance.sol
+++ contracts/StrategyYFIGovernance.sol
@@ -87,10 +87,11 @@ interface Zap {
 
 
 /**
- *本策略操作的资金是YFI
- *yearn vaults中的YFI vault:使用者存入YFI代币，获得yYFI代币，赎回yYFI代币时可获取原本投入的YFI代币加上策略赚取的YFI代币;
- *本合约是YFI vault的策略合约，合约收到YFI后，将YFI stake到ygov.finance,从而赚取收益;
- *收益来源：yearn v2 机枪池所收到的费用去了专门的国库合约（限额50万美元），超过限额将会自动到治理合约,stake YFI到治理合约能赚取这部分收益;
+ *本策略合约操作的资金是YFI；
+ *用户角度：在Yearn Vaults中的YFI Vault，使用者存入YFI代币，获得yYFI代币，赎回yYFI代币时可获取原本投入的YFI代币加上策略赚取的YFI代币；
+ *Yearn V2角度：存入的YFI先到YFI Vault中，再到对应的控制器合约中，由控制器合约指定策略合约对defi项目进行投资；
+ *本合约是YFI Vault的策略合约，策略合约收到YFI后，将YFI stake到yfi gov项目,从而赚取收益;
+ *收益来源：yearn v2 机枪池所收到的费用去了专门的国库合约（限额50万美元），超过限额将会自动到治理合约,stake YFI到yfi gov能赚取这部分收益；
  *YFI策略合约 地址:0x395F93350D5102B6139Abfc84a7D6ee70488797C 有小部分更新 暂时没拉下来;
  */
 contract StrategyYFIGovernance {
@@ -134,7 +135,7 @@ contract StrategyYFIGovernance {
 
     //5%的绩效费用
     uint256 public fee = 500;
-    //最大值100% 各项费率基准值
+    //100% 各项费率基准值
     uint256 public constant max = 10000;
 
     //治理地址:用于治理权限检验
@@ -146,7 +147,7 @@ contract StrategyYFIGovernance {
 
     /**
      *@dev    构造函数，初始化时调用，部署合约时，只设置一个控制器地址，其他默认为部署地址；
-     *@param  控制器地址;
+     *@param _controller 控制器地址;
      */
     constructor(address _controller) public {
         governance = msg.sender;
@@ -175,19 +176,20 @@ contract StrategyYFIGovernance {
     }
 
     /**
-     *@dev 存款处理方法
+     *@dev 存款处理方法 把本合约中的YFI stake到yfi gov中用以赚取stake收益
      */
     function deposit() public {
-        //授权使用合约中的YFI代币
+        //授权yfi gov;
         IERC20(want).safeApprove(gov, 0);
         IERC20(want).safeApprove(gov, IERC20(want).balanceOf(address(this)));
-        //把YFI stake 到governance 治理合约
+        //把YFI stake 到yfi gov
         Governance(gov).stake(IERC20(want).balanceOf(address(this)));
     }
 
     /**
      *Controller only function for creating additional rewards from dust
-     *把某一个token在本合约的余额全部取回到controller控制器合约
+     *@dev 把某token(非YFI)在本合约的余额全部取回到controller控制器合约
+     *param _asset 某token
      */
     function withdraw(IERC20 _asset) external returns (uint256 balance) {
         //确保是控制器合约调用
@@ -202,7 +204,8 @@ contract StrategyYFIGovernance {
 
     /**
      *Withdraw partial funds, normally used with a vault withdrawal
-     *取款方法，通常是用户从Vault取款时，Vault合约余额不够时触发
+     *@dev 取款方法，通常是用户从Vault取款时，Vault合约余额不够时触发，赎回数额先到策略合约，再到控制器合约，最后由控制器合约发送至vault合约供用户提取；
+     *@param _amount 数额
      */
     function withdraw(uint256 _amount) external {
         //确保是控制器合约调用
@@ -211,26 +214,26 @@ contract StrategyYFIGovernance {
         uint256 _balance = IERC20(want).balanceOf(address(this));
         //如果本合约余额不够
         if (_balance < _amount) {
-            //赎回不够的金额
+            //赎回不够的数额
             _amount = _withdrawSome(_amount.sub(_balance));
-            //实际赎回金额 + balance = 真实的用金额
+            //赎回数额加上本合约已有的余额 
             _amount = _amount.add(_balance);
         }
         //计算取款收费
         uint256 _fee = _amount.mul(fee).div(max);
-        //将收到费发给奖励池-----通过controller获取相应的地址
+        //将收到的费用发给奖励池，通过controller获取相应的地址
         IERC20(want).safeTransfer(Controller(controller).rewards(), _fee);
-        //从控制器合约获取保险柜地址
+        //从控制器合约获取vault地址
         address _vault = Controller(controller).vaults(address(want));
-        //检验保险地址是否有误
+        //检验vault地址是否有误
         require(_vault != address(0), "!vault"); // additional protection so we don't burn the funds
-        //扣除费用后，发送回保险柜地址
+        //扣除费用后，发送回vault地址
         IERC20(want).safeTransfer(_vault, _amount.sub(_fee));
     }
 
     /**
      *Withdraw all funds, normally used when migrating strategies
-     *全部取款方法，通常是停止策略或者切换策略时调用
+     *@dev 全部取款方法，通常是停止策略或者切换策略时调用
      */
     function withdrawAll() external returns (uint256 balance) {
         //确保是控制器合约调用
@@ -239,16 +242,16 @@ contract StrategyYFIGovernance {
         _withdrawAll();
         //取YFI的全部余额
         balance = IERC20(want).balanceOf(address(this));
-        //获取保险柜地址
+        //获取vault地址
         address _vault = Controller(controller).vaults(address(want));
-        //检验保险地址是否有误
+        //检验vault地址是否有误
         require(_vault != address(0), "!vault"); // additional protection so we don't burn the funds
-        //将全部的YFI发送回保险柜
+        //将全部的YFI发送回vault金库
         IERC20(want).safeTransfer(_vault, balance);
     }
 
     /**
-     *@dev 从投资中全部赎回
+     *@dev 把yfi gov中的投资全部赎回至本策略合约
      */
     function _withdrawAll() internal {
         Governance(gov).exit();
@@ -256,18 +259,18 @@ contract StrategyYFIGovernance {
 
     /**
      *@dev 收获方法
-     *将治理合约中奖励的yCrv,先在curve的y池移除流动性换成usdt
-     *再在uniswap中把usdt换成weth，weth再换成YFI，YFI继续stake到治理合约中
+     *把在yfi gov中奖励的yCrv,先在curve的y池移除流动性换成usdt
+     *再在uniswap中把usdt换成weth，weth再换成YFI，YFI继续stake到yfi gov中
      */
     function harvest() public {
-        //确保是策略管理员或治理员或tx.origin调用
+        //确保是策略管理员或治理员或交易的发送者调用
         require(
             msg.sender == strategist ||
                 msg.sender == governance ||
                 msg.sender == tx.origin,
             "!authorized"
         );
-        //执行收获操作 从治理合约收获stake产生的奖励,奖励为yCrv
+        //执行收获操作 从yfi gov收获stake产生的奖励 奖励为yCrv
         Governance(gov).getReward();
         //获取合约中yCrv的余额
         uint256 _balance = IERC20(reward).balanceOf(address(this));
@@ -275,7 +278,7 @@ contract StrategyYFIGovernance {
             //授权curve的y池
             IERC20(reward).safeApprove(zap, 0);
             IERC20(reward).safeApprove(zap, _balance);
-            //在curve的y池中移除流动性，把yCrv换成usdt
+            //在curve的y池中移除流动性，即yCrv换成usdt
             Zap(zap).remove_liquidity_one_coin(_balance, 2, 0);
         }
         //获取合约中usdt的余额
@@ -299,7 +302,7 @@ contract StrategyYFIGovernance {
             );
         }
         if (IERC20(want).balanceOf(address(this)) > 0) {
-            //如果余额还有YFI，继续存款;
+            //如果余额还有YFI，继续stake到yfi gov赚取收益;
             deposit();
         }
     }
@@ -310,6 +313,7 @@ contract StrategyYFIGovernance {
      *param _amount 赎回数量
      */
     function _withdrawSome(uint256 _amount) internal returns (uint256) {
+        //把在yfi gov中的投资赎回
         Governance(gov).withdraw(_amount);
         return _amount;
     }
@@ -323,14 +327,14 @@ contract StrategyYFIGovernance {
     }
 
     /**
-     *@dev 治理合约的余额
+     *@dev yfi gov的余额
      */
     function balanceOfYGov() public view returns (uint256) {
         return Governance(gov).balanceOf(address(this));
     }
 
     /**
-     *@dev 本策略管理的总YFI金额
+     *@dev 本合约的YFI余额 加上 yfi gov的余额 等于 管理总量
      */
     function balanceOf() public view returns (uint256) {
         return balanceOfWant().add(balanceOfYGov());
@@ -357,6 +361,7 @@ contract StrategyYFIGovernance {
     }
     /**
      *@dev 重新设置治理地址
+     *@param _governance 治理地址
      */
     function setGovernance(address _governance) external {
         //确保是治理地址调用
@@ -365,6 +370,7 @@ contract StrategyYFIGovernance {
     }
     /**
      *@dev 重新设置控制器地址
+     *param _controller 控制器地址
      */
     function setController(address _controller) external {
         //确保是治理地址调用
